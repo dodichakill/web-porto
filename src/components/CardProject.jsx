31425bf4d904c2a3bc1ba7d44f563d2af20392e8
dodichakill <dodiherlangga91@gmail.com>
2024-07-26 08:03:17 +0700

    update styling img in cardProjects

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -10,23 +10,27 @@ function CardProject({
   link,
 }) {
   return (
-    <div className="bg-gradient-to-tr shadow-blue-300 transition hover:shadow-blue-400 from-blue-50 to-blue-100 text-white shadow-lg p-5 rounded-xl">
-      <Image src={image} alt={"project " + name} className="w-full shadow-sm" />
-      <h2 className="text-3xl font-semibold mt-5 font-sans text-blue-900">
+    <div className="bg-gradient-to-tr shadow-blue-300 overflow-x-hidden transition hover:shadow-blue-400 from-blue-50 to-blue-100 text-white shadow-lg pt-3 p-4 rounded-xl">
+      <Image
+        src={image}
+        alt={"project " + name}
+        className="w-full shadow-sm scale-110 rounded transition-all"
+      />
+      <h2 className="text-3xl p-2 font-semibold mt-5 font-sans text-blue-900">
         {name}
       </h2>
-      <div className="font-sans text-lg mt-3 text-slate-600">{desc}</div>
+      <div className="font-sans p-2 text-lg text-slate-600">{desc}</div>
 
-      <div className="flex justify-between">
+      <div className="flex p-2 justify-between">
         {link && (
           <Link
             href={link}
-            className="mt-5 hover:text-white transition-all hover:bg-blue-500 text-blue-500 border-2 border-blue-300 font-semibold text-lg sm:text-xl font-sans bg-white px-5 py-3 rounded-xl flex items-center gap-1 sm:gap-3"
+            className="mt-3 hover:text-white transition-all hover:bg-blue-500 text-blue-500 border-2 border-blue-300 font-semibold text-lg sm:text-xl font-sans bg-white px-5 py-3 rounded-xl flex items-center gap-1 sm:gap-3"
           >
             <FaEye /> View
           </Link>
         )}
-        <div className="mt-5 flex gap-5 text-xl sm:text-3xl text-blue-900 p-3">
+        <div className="mt-3 flex gap-5 text-xl sm:text-3xl text-blue-900 p-3">
           {icTech}
         </div>
       </div>
