bdd494284c08cfd3fbda077f13380033953a334b
dodichakill <dodiherlangga91@gmail.com>
2024-08-11 13:25:35 +0700

    update page about

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,7 +18,7 @@ export default function Home() {
       <Navbar />
       <main className="w-[95%] mx-auto mt-32 mb-10">
         <div
-          className="flex flex-col-reverse lg:flex-row bg-gradient-to-tr from-sky-500 shadow-xl shadow-blue-300 to-blue-600 rounded-xl text-white gap-10 p-5 lg:px-10 lg:py-24 justify-evenly items-center"
+          className="flex flex-col-reverse lg:flex-row bg-gradient-to-tr from-sky-500 shadow-xl shadow-blue-300 to-blue-600 rounded-xl text-white gap-10 p-5 pb-10 lg:px-10 lg:py-24 justify-evenly items-center"
           data-aos="fade-up"
           data-aos-delay="300"
         >
@@ -27,27 +27,29 @@ export default function Home() {
             data-aos="fade-left"
             data-aos-delay="500"
           >
-            <h1 className="text-3xl md:text-5xl font-bold font-sans">
+            <h1 className="text-3xl md:text-5xl text-center md:text-left font-bold font-sans">
               Hi, im Dodi <br />
               Software Developer
             </h1>
 
-            <p className="mt-6 md:text-lg font-mono">
+            <p className="mt-6 md:text-lg font-mono text-center md:text-left">
               Software Engineer specializing in Frontend Development. Strong
               analytical and creative problem-solving skills honed through years
               of coding. IT Enthusiastic seeking to leverage expertise for
               impactful solutions.
             </p>
 
-            <Link
-              href="https://bit.ly/MyResumeATS"
-              className="resume mt-5 transition-all hover:bg-blue-300 hover:text-white inline-block text-blue-500 font-semibold text-xl font-sans bg-white px-5 py-4 rounded-xl"
-              target="_blank"
-            >
-              <span className="flex gap-2 items-center">
-                <FaRegNewspaper /> Check out My Resume
-              </span>
-            </Link>
+            <div className="flex items-center md:items-start">
+              <Link
+                href="https://bit.ly/MyResumeATS"
+                className="resume mx-auto md:mx-0 mt-5 transition-all hover:bg-blue-300 hover:text-white inline-block text-blue-500 font-semibold text-xl font-sans bg-white px-5 py-4 rounded-xl"
+                target="_blank"
+              >
+                <span className="flex gap-2 items-center">
+                  <FaRegNewspaper /> Check out My Resume
+                </span>
+              </Link>
+            </div>
           </section>
           <Image
             src={imgProfile}
