466c34e139b13f1db980359674442d99ffd3f6e7
dodichakill <dodiherlangga91@gmail.com>
2024-08-11 13:16:50 +0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { FaHeart, FaRegCopyright } from "react-icons/fa6";
 
 function Footer() {
   return (
-    <footer className="w-[95%] m-auto mt-10 p-6 bg-gradient-to-r from-blue-400 to-blue-500 rounded-xl text-white font-sans text-xl font-semibold text-center flex items-center justify-center shadow-lg shadow-blue-300">
+    <footer className="w-[95%] m-auto mt-10 p-6 bg-gradient-to-r from-blue-400 to-blue-500 rounded-xl text-white font-sans text-lg md:text-xl text-center flex items-center justify-center shadow-lg shadow-blue-300">
       <FaRegCopyright className="mr-2" /> 2024 - built with{" "}
       <FaHeart className="text-white mx-2" /> by{" "}
       <Link
